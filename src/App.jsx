c58c66c376e13d91ac934bd5ded6b780e6c6846d
Sojib Islam <sojibislam6505@gmail.com>
2025-12-01 00:06:39 +0600

    Update source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router"
-import Home from "./pages/Home"
-import Shop from "./pages/Shop"
-import Layout from "./components/Layout/Layout"
-import ProductDetails from "./components/ProductDetails"
-import Contact from "./pages/Contact"
+
+import { Routes, Route } from "react-router-dom"; 
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Layout from "./components/Layout/Layout";
+import ProductDetails from "./components/ProductDetails";
+import Contact from "./pages/Contact";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element ={<Home/>}/>
-            <Route path="/shop" element ={<Shop/>}/>
-            <Route path="/shop/:id" element ={<ProductDetails/>}/>
-            <Route path="/contact" element={<Contact/>}/>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Home page */}
+        <Route index element={<Home />} />
 
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+        {/* Shop page */}
+        <Route path="shop" element={<Shop />} />
+
+        {/* Product details page */}
+        <Route path="shop/:id" element={<ProductDetails />} />
+
+        {/* Contact page */}
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
